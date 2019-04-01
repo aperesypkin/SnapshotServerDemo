b672eb8b4ca449c5aa41406d81fef734a29dd8eb
Alexander Peresypkin <peresypkin10@gmail.com>
2019-04-01 21:42:24 +0300

    configured heroku database

--- Sources/App/Setup/databases.swift
+++ Sources/App/Setup/databases.swift
@@ -10,18 +10,36 @@ import FluentPostgreSQL
 
 public func databases(config: inout DatabasesConfig) throws {
     
-    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
-    let username = Environment.get("DATABASE_USER") ?? "postgres"
-    let databaseName = Environment.get("DATABASE_DB") ?? "snapshot_server"
-    let password = Environment.get("DATABASE_PASSWORD") ?? "123456"
+    let databaseConfig: PostgreSQLDatabaseConfig
+    if let url = Environment.get("DATABASE_URL") {
+        databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+    } else {
+        let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+        let username = Environment.get("DATABASE_USER") ?? "postgres"
+        let databaseName = Environment.get("DATABASE_DB") ?? "snapshot_server"
+        let password = Environment.get("DATABASE_PASSWORD") ?? "123456"
+        let databasePort = 5432
+        
+        databaseConfig = PostgreSQLDatabaseConfig(
+            hostname: hostname,
+            port: databasePort,
+            username: username,
+            database: databaseName,
+            password: password)
+    }
     
-    let pgConfig = PostgreSQLDatabaseConfig(hostname: hostname,
-                                            port: 5432,
-                                            username: username,
-                                            database: databaseName,
-                                            password: password)
+//    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+//    let username = Environment.get("DATABASE_USER") ?? "postgres"
+//    let databaseName = Environment.get("DATABASE_DB") ?? "snapshot_server"
+//    let password = Environment.get("DATABASE_PASSWORD") ?? "123456"
+//
+//    let pgConfig = PostgreSQLDatabaseConfig(hostname: hostname,
+//                                            port: 5432,
+//                                            username: username,
+//                                            database: databaseName,
+//                                            password: password)
     
-    let database = PostgreSQLDatabase(config: pgConfig)
+    let database = PostgreSQLDatabase(config: databaseConfig)
     
     config.add(database: database, as: .psql)
     
